57b089ef8daec137de508fe8c0c9a72791c90b1b
robertobaiochi <robertombbaiochi@gmail.com>
2024-06-27 12:53:20 -0300

    refactor: default server port

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,6 @@ import createApp from "./app";
 const app = createApp();
 const port = process.env.SERVER_PORT;
 
-app.listen(port, () => {
+app.listen(port || 3333, () => {
     console.log(`🔥 Server running in http://localhost:${port}`);
 });
